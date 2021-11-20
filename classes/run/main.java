4c5cf3afc4be0248c025ef6b9cc961be53e7b899
DmitriBazilov <90473311+DmitriBazilov@users.noreply.github.com>
2021-11-20 14:13:21 +0300

    Update main.java

--- classes/run/main.java
+++ classes/run/main.java
@@ -5,12 +5,19 @@ import ru.ifmo.se.pokemon.*;
 
 public class main {
 	public static void main(String[] args) {
-		Battle b = new Battle();
-		Pokemon sawk = new Sawk("", 10);
-		Pokemon chingling = new Chingling("", 10);
-		b.addAlly(sawk);
-		b.addFoe(chingling);
-		b.go();
-
+                Battle b = new Battle();
+                Pokemon sawk = new Sawk("", 10);
+                Pokemon chingling = new Chingling("", 10);
+                Pokemon chimecho = new Chimecho("", 10);
+                Pokemon solosis = new Solosis("", 10);
+                Pokemon duosion = new Duosion("", 10);
+                Pokemon reuniclus = new Reuniclus("", 10);
+                b.addAlly(sawk);
+                b.addAlly(chimecho);
+                b.addAlly(duosion);
+                b.addFoe(reuniclus);
+                b.addFoe(solosis);
+                b.addFoe(chingling);
+                b.go();
 	}
 }
